c5ac5cd7949a89a9254d2dc45698a8b376998d6b
Kevin Moen <kevmoesto@gmail.com>
2021-03-29 15:56:05 +0200

    Testing Wadersons GUI

--- python/ADS/testing/main.py
+++ python/ADS/testing/main.py
@@ -1,3 +1,4 @@
+from re import S
 from python.ADS.testing.pyadstest import DRIVETIME
 import pyads
 import logging
@@ -14,15 +15,15 @@ class MotorController(object):
         self.plc = pyads.Connection(self.AMS_ADDRESS, self.PORT)
         self.logging.info('Connected established.')
     
-    def start(self):
+    def start(self) -> None:
         self.plc.start()
         self.logging.info('Started')
     
-    def stop(self):
+    def stop(self) -> None:
         self.plc.stop()
         self.logging.info('Stopped')
     
-    def getSymbols(self):
+    def getSymbols(self) -> None:
         ################################################################
         # Horizontal Axis 
         self.hEnable      = self.plc.get_symbol('HAVL.enable')
@@ -37,83 +38,112 @@ class MotorController(object):
         self.hBackward    = self.plc.get_symbol('RAVL.backwards')
         self.hResetError  = self.plc.get_symbol('RAVL.resetError')
     
-    def readHorizontalAxis(self):
+    def readHorizontalAxis(self) -> None:
         self.logging.info('Hor. enable\t',       self.hEnable.read())  
         self.logging.info('Hor. forward\t',      self.hForward.read())
         self.logging.info('Hor. backward\t',     self.hBackward.read())
         self.logging.info('Hor. reseterror\t',   self.hResetError.read())
     
-    def readRotationalAxis(self):
+    def readRotationalAxis(self) -> None:
         self.logging.info('Rot. enable\t',       self.rEnable.read())  
         self.logging.info('Rot. forward\t',      self.rForward.read())
         self.logging.info('Rot. backward\t',     self.rBackward.read())
         self.logging.info('Rot. reseterror\t',   self.rResetError.read()) 
     
-    def resetHorizontalAxis(self):
+    def resetHorizontalAxis(self) -> None:
         self.hResetError.write(True)
         sleep(self.CMDDELAY)
         self.hResetError.write(False)
         
-    def resetRotationalAxis(self):
+    def resetRotationalAxis(self) -> None:
         self.rResetError.write(True)
         sleep(self.CMDDELAY)
         self.rResetError.write(False)
         
-    def enableHorizontalAxis(self):
+    def enableHorizontalAxis(self) -> None:
         self.hEnable(True)
         self.logging.info("Horizontal Axis:\t ENABLED")
     
-    def disableHorizontalAxis(self):
+    def disableHorizontalAxis(self) -> None:
         self.hEnable(False)
         self.logging.info("Horizontal Axis:\t DISABLED")
         
-    def enableRotationalAxis(self):
+    def enableRotationalAxis(self) -> None:
         self.rEnable(True)
         self.logging.info("Rotational Axis:\t ENABLED")
     
-    def disableRotationalAxis(self):
+    def disableRotationalAxis(self) -> None:
         self.rEnable(False)
         self.logging.info("Rotational Axis:\t DISABLED")
     
-    def moveHorizontalForward(self):
+    def startExtendSnake(self) -> None:
         self.hForward(True)
         self.logging.info("Horizontal Moving:\t FORWARD")
         
-    def stopHorizontalForward(self):
+    def stopExtendSnake(self) -> None:
         self.hForward(False)
         self.logging.info("Horizontal Stopped")
         
-    def moveHorizontalBackward(self):
+    def startRetractSnake(self) -> None:
         self.hBackward(True)
         self.logging.info("Horizontal Moving:\t BACKWARD")
         
-    def stopHorizontalBackward(self): 
+    def stopRetractSnake(self) -> None:
         self.hBackward(False)
         self.logging.info("Horizontal Stopped")
         
-    def moveRotationClockwise(self):
+    def moveRotationClockwise(self) -> None:
         self.rForward(True)
         self.logging.info("Rotation Moving:\t CLOCKWISE")
         
-    def stopRotationClockwise(self):
+    def stopRotationClockwise(self) -> None:
         self.rForward(False)
         self.logging.info("Rotation Stopped")
         
-    def moveRotationCounterClockwise(self):
+    def moveRotationCounterClockwise(self) -> None:
         self.rBackward(True)
         self.logging.info("Rotation Moving:\t COUNTER CLOCKWISE")
         
-    def stopRotationCounterClockwise(self): 
+    def stopRotationCounterClockwise(self) -> None:
         self.rBackward(False)
         self.logging.info("Rotation Stopped")
     
-    def stopHorizontal(self):
+    def stopHorizontal(self) -> None:
         self.hForward(False)
         self.hBackward(False)
         self.logging.info("Horizontal Stopped")
         
-    def stopRotation(self):
+    def stopRotation(self) -> None:
         self.rforward(False)
         self.rBackward(False)
         self.logging.info("Rotation Stopped")
+    
+    def runTest(self) -> None:
+        self.start()
+        self.getSymbols()
+        self.readHorizontalAxis()
+        self.readRotationalAxis()
+        self.resetHorizontalAxis()
+        self.resetRotationalAxis()
+        self.enableHorizontalAxis()
+        self.enableRotationalAxis()
+        self.startExtendSnake()
+        sleep(1)
+        self.stopExtendSnake()
+        sleep(0.5)
+        self.startRetractSnake()
+        sleep(1)
+        self.stopRetractSnake()
+        sleep(0.5)
+        self.moveRotationCounterClockwise()
+        sleep(1)
+        self.stopRotationCounterClockwise()
+        sleep(0.5)
+        self.moveRotationClockwise()
+        sleep(1)
+        self.stopRotationClockwise()
+        sleep(0.5)
+        self.disableHorizontalAxis()
+        self.disableRotationalAxis()
+        self.stop()
         
